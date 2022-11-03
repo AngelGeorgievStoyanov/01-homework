5e1340dddf85cfbf4e4625c502b86690416b21ac
Angel <angelo_stoyanov@abv.bg>
2022-11-03 18:25:27 +0200

    update date

--- js/promises.js
+++ js/promises.js
@@ -8,7 +8,9 @@ let div = document.getElementById('result');
 let arr = [];
 let bookDetails;
 div.addEventListener('click', (e) => details(e));
-btnSearch.addEventListener('click', (e) => init(query.value, e));
+btnSearch.addEventListener('click', (e) => { if (query.value.trim() != '') { init(query.value.trim(), e) } });
+
+
 
 function init(query, e) {
     e.preventDefault()
@@ -34,23 +36,31 @@ function init(query, e) {
 };
 
 function detailsPage(bookObj) {
-   
     let idBook = bookObj.id;
 
     let article = elem('article', false, false, false, false, false, idBook);
 
     let h1 = elem('h1');
-    h1.textContent ='Title  of the Book  --- '+ bookObj.volumeInfo.title;
+    h1.textContent = 'Title  of the Book  --- ' + bookObj.volumeInfo.title;
 
     let h2 = elem('h2');
-    h2.textContent =bookObj.volumeInfo.authors !=undefined ? 'Authors of the Book  ---  '+ bookObj.volumeInfo.authors.join('  and  ') : 'Тhere is no author for this book in DB' ;
+    h2.textContent = bookObj.volumeInfo.authors != undefined ? 'Authors of the Book  ---  ' + bookObj.volumeInfo.authors.join('  and  ') : 'Тhere is no author for this book in DB';
 
     let h3 = elem('h3');
-    let date = bookObj.volumeInfo.publishedDate.split('-').reverse().join('/');
-    h3.textContent = date != undefined ?'Published date  ---  ' +date : 'No info for published date';
+    let date = bookObj.volumeInfo.publishedDate
+    if (date != undefined) {
+
+        date = 'Published date  ---  ' + date.split('-').reverse().join('/');
+
+    } else {
+
+        date = 'No info for published date';
+    }
+
+    h3.textContent = date;
 
     let p = elem('p');
-    p.innerHTML = bookObj.volumeInfo.description !=undefined?'Description  '+bookObj.volumeInfo.description:'Тhere is no description for this book in DB';
+    p.innerHTML = bookObj.volumeInfo.description != undefined ? 'Description  ' + bookObj.volumeInfo.description : 'Тhere is no description for this book in DB';
 
     let src = bookObj.volumeInfo.imageLinks.thumbnail;
     let img = elem('img', false, false, false, false, src);
